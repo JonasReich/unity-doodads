e7e456ee997b1fd74fbb585668190a474aceb4c5
Jonas Reich <joreich@stud.mediadesign.de>
2017-05-17 00:30:35 +0200

    Reduce redundancy of LayerMask extension methods

--- Assets/Scripts/Extensions/LayerMask.cs
+++ Assets/Scripts/Extensions/LayerMask.cs
@@ -8,23 +8,11 @@ namespace UnityDoodats
 	public static class LayerMaskExtensions
 	{
 		/// <summary>
-		/// Check whether a given layer is part of a LayerMask
+		/// Check whether a given layer is part of the LayerMask
 		/// </summary>
-		/// <param name="mask">LayerMask to test against</param>
-		/// <param name="layer">Layer to test</param>
-		public static bool IsInLayerMask (this LayerMask mask, int layer)
+		public static bool Contains (this LayerMask mask, int layer)
 		{
 			return ((mask.value & (1 << layer)) > 0);
-		}
-
-		/// <summary>
-		/// Check whether a given GameObject is part of a LayerMask
-		/// </summary>
-		/// <param name="mask">LayerMask to test against</param>
-		/// <param name="layer">GameObject to test</param>
-		public static bool IsInLayerMask (this LayerMask mask, GameObject obj)
-		{
-			return ((mask.value & (1 << obj.layer)) > 0);
 		}
 	}
 }
